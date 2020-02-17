244db6564ab178b27d7a807639d2d085e5975345
Lis <andrei_chernov1@epam.com>
2020-02-17 20:04:08 +0100

    blinkers ready

---
+++ src/tmp/blink_5.c
@@ -0,0 +1,26 @@
+#define F_CPU 16000000UL // 16 MHz
+ 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+// DDR Data Direction Register
+#define SET_DDR_OUT(DDR, PIN) DDR |= _BV(PIN)
+
+// PORTS Data Register 
+#define TOGGLE(PORTS, PIN) PORTS ^= _BV(PIN);
+
+// DEFINE PINS
+#define LED_PIN PD5
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+
+int main(void){
+ 
+  SET_DDR_OUT(LED_DDR, LED_PIN);
+  while(1){
+    TOGGLE(LED_PORT, LED_PIN);
+    _delay_ms (5000);
+  }
+  return 0;
+}
