9ee1d52e8e3d039f7d9365f78640303394da4de9
Lis <andrei_chernov1@epam.com>
2020-02-20 19:38:10 +0100

    it works but doesn't start

--- src/sdcard_read/boot.c
+++ src/sdcard_read/boot.c
@@ -2,28 +2,23 @@
 
 int main(void){
   setup_button();
-  /*
+  setup_led();
+
   // MAIN LOGIC
-  while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
+  if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
+    SET_HIGH(LED_PORT, LED_PIN);
+    while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){};
+    SET_LOW(LED_PORT, LED_PIN);
     load(BOOT_APP);
   }else{
-    goto (void*) 0;
-  }
-  return 0;
-  */
-
-  // FIXME DEBUG
-  load(BOOT_APP);
-  while(1){
+    goto *start_address;
   }
   return 0;
 }
 
 void load(const char* file_path){
   setup_led();
-
-  displayBegin();
-  displayClean();
+  ADDRESS = 0;
 
   if (!sd_init()){
     error_light();
@@ -34,20 +29,54 @@ void load(const char* file_path){
     error_blink();
   }
 
-  uint8_t buf[SPM_PAGESIZE];
   while(boot_file.cursor < boot_file.size){
-    if (!file_read(&boot_file, buf, SPM_PAGESIZE)){
+    if (!file_read(&boot_file, PAGE_BUFFER, SPM_PAGESIZE)){
       error_blink();
     }
-    // make a page
+    block_flash_load();
   }
   SET_HIGH(LED_PORT, LED_PIN);
   _delay_ms(100);
   SET_LOW(LED_PORT, LED_PIN);
 
-  // FIXME
-  //reboot();
+  reboot();
  }
+
+void block_flash_load(void){
+	uint16_t temp_address = ADDRESS;	
+	uint16_t i, temp_word;
+
+	// Perform page erase
+  //boot_page_erase(address);
+
+	// Wait until the memory is erased
+  //boot_spm_busy_wait();		
+
+	// fill the flash page buffer with the data
+  for(i = 0; i < SPM_PAGESIZE; i+=2){
+		// load the little end byte from the word
+		temp_word = PAGE_BUFFER[i];
+
+		// load the big end byte
+		temp_word += (PAGE_BUFFER[i+1] << 8);
+		
+		// put the word into the page butter
+    boot_page_fill(ADDRESS, temp_word);;
+    	
+		// incrememnt the word address						
+    ADDRESS += 2;
+  } 
+    
+	// write the page to flash
+  boot_page_write(temp_address);
+
+	// wait until finished writing
+  boot_spm_busy_wait();
+	
+	// Re-enable the RWW section 
+  //boot_rww_enable();
+ 
+}
 
 void setup_button(void){
   SET_DDR_IN(BUTTON_C_DDR, BUTTON_C_PIN);
--- src/sdcard_read/boot.h
+++ src/sdcard_read/boot.h
@@ -1,18 +1,21 @@
+#include <avr/boot.h>
 #include <util/delay.h>
 #include <avr/io.h>
 #include "macro.h"
 #include "pins.h"
-#include "display_oled_i2c/display.h"
 #include "sd_spi/sd.h"
-
+                                                                               
 #ifndef BOOT_H                                         
 #define BOOT_H
-                                                                                
+
 const char BOOT_APP[] PROGMEM = "/BIN/BOOT.BIN";
-//const char BOOT_APP[] PROGMEM = "/SAVE/BOOT.BIN";
+void* start_address = 0;
+
 uint8_t PAGE_BUFFER[SPM_PAGESIZE];
+uint32_t ADDRESS;
 
 void load(const char* file_path);
+void block_flash_load();
 void setup_button(void);
 void setup_led(void);
 void error_light(void);
