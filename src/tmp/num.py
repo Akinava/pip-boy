380c9c28474c1050d88cfa7b93e9283dc5d6b539
Lis <andrei_chernov1@epam.com>
2020-02-23 15:07:52 +0100

    clean project

--- src/tmp/num.py
+++
@@ -1,9 +0,0 @@
-with open('number.txt', 'r') as f:
-    data = f.read().split('\n')[:-1]
-    
-    for x in range(3):
-        n = 0
-        for d_ind in range(len(data)):
-            n += (int(data[d_ind][x])<<d_ind)
-        print(hex(n))
-
