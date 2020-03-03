e266a0b487f3dcf521fa2d0677472f155c31ada9
Lis <andrei_chernov1@epam.com>
2020-03-03 12:50:39 +0100

    wdt still has an issue

--- src/tmp/watchdog/boot.c
+++ src/tmp/watchdog/boot.c
@@ -11,24 +11,41 @@ void setup_led_(void){
 }
 
 void watchdogConfig(uint8_t x) {
-  WDTCSR = _BV(WDCE) | _BV(WDE);
+  //WDTCSR = _BV(WDCE) | _BV(WDE);
+  WDTCSR = x;
 }
 
 int main(void){
   setup_button_();
   setup_led_();
+
+  // MCUSR ??
+  /*
+  displayBegin();
+  displayClean();
+  //WDTCSR    // 0
+  //_BV(WDP2) | _BV(WDP1) | _BV(WDE) // 0x0e 1s
+  //_BV(WDP1) | _BV(WDP0) | _BV(WDE) // 0x0b 125ms
+  //WDCE      // 4
+  //WDE       // 3
+
+  displayUpdate();
+  while(1);
+  */
   
-  wdt_reset();
-  wdt_disable(); 
-  //watchdogConfig(0); // WATCHDOG_OFF
+  
+  //wdt_reset();
+  //wdt_disable(); 
+  watchdogConfig(0); // WATCHDOG_OFF
   if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
     //wdt_enable(WDTO_1S);
+    watchdogConfig(_BV(WDP2) | _BV(WDP1) | _BV(WDE)); // WATCHDOG_1S
 
     SET_HIGH(LED_PORT, LED_PIN);
     while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN));
   }
   SET_HIGH(LED_PORT, LED_PIN);
-  //watchdogConfig(_BV(WDP1) | _BV(WDP0) | _BV(WDE)); // WATCHDOG_125MS
-  wdt_enable(WDTO_120MS);
+  watchdogConfig(0xb); // WATCHDOG_125MS
+  //wdt_enable(WDTO_120MS);
   while (1);
 }
