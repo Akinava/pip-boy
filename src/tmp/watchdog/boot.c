c57ec1b35f712e828afe8f8358ba3e10a24c05f6
Lis <andrei_chernov1@epam.com>
2020-03-03 20:34:00 +0100

    wdt works / the chip is shit

--- src/tmp/watchdog/boot.c
+++ src/tmp/watchdog/boot.c
@@ -10,42 +10,23 @@ void setup_led_(void){
   SET_LOW(LED_PORT, LED_PIN);
 }
 
-void watchdogConfig(uint8_t x) {
-  //WDTCSR = _BV(WDCE) | _BV(WDE);
-  WDTCSR = x;
-}
-
 int main(void){
   setup_button_();
   setup_led_();
 
-  // MCUSR ??
-  /*
-  displayBegin();
-  displayClean();
-  //WDTCSR    // 0
-  //_BV(WDP2) | _BV(WDP1) | _BV(WDE) // 0x0e 1s
-  //_BV(WDP1) | _BV(WDP0) | _BV(WDE) // 0x0b 125ms
-  //WDCE      // 4
-  //WDE       // 3
+  // WATCHDOG_OFF
+  //WDTCSR = _BV(WDCE) | _BV(WDE);
+  WDTCSR = 0;
 
-  displayUpdate();
-  while(1);
-  */
-  
-  
-  //wdt_reset();
-  //wdt_disable(); 
-  watchdogConfig(0); // WATCHDOG_OFF
   if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
-    //wdt_enable(WDTO_1S);
-    watchdogConfig(_BV(WDP2) | _BV(WDP1) | _BV(WDE)); // WATCHDOG_1S
-
     SET_HIGH(LED_PORT, LED_PIN);
     while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN));
   }
   SET_HIGH(LED_PORT, LED_PIN);
-  watchdogConfig(0xb); // WATCHDOG_125MS
-  //wdt_enable(WDTO_120MS);
+
+  // WATCHDOG_125MS
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  WDTCSR = _BV(WDP1) | _BV(WDP0) | _BV(WDE);
+
   while (1);
 }
