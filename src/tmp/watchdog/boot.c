a51997f821dd773b9713209f02e7332e7d0ce8c5
Lis <andrei_chernov1@epam.com>
2020-03-03 12:07:37 +0100

    wdt doesn't work

--- src/tmp/watchdog/boot.c
+++ src/tmp/watchdog/boot.c
@@ -1,26 +1,5 @@
 #include "boot.h"
 
-int main(void){
-  setup_button_();
-  setup_led_();
-  
-  //displayBegin();
-  //displayClean();
-
-  wdt_reset();
-  wdt_disable(); 
-  if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
-    //wdt_enable(WDTO_1S);
-
-    SET_HIGH(LED_PORT, LED_PIN);
-    while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN));
-  }
-  SET_HIGH(LED_PORT, LED_PIN);
-  wdt_enable(WDTO_120MS);
-  while (1);
-
-}
-
 void setup_button_(void){
   SET_DDR_IN(BUTTON_C_DDR, BUTTON_C_PIN);
   SET_PULLUP(BUTTON_C_PORT, BUTTON_C_PIN);
@@ -29,4 +8,27 @@ void setup_button_(void){
 void setup_led_(void){
   SET_DDR_OUT(LED_DDR, LED_PIN);
   SET_LOW(LED_PORT, LED_PIN);
+}
+
+void watchdogConfig(uint8_t x) {
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+}
+
+int main(void){
+  setup_button_();
+  setup_led_();
+  
+  wdt_reset();
+  wdt_disable(); 
+  //watchdogConfig(0); // WATCHDOG_OFF
+  if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
+    //wdt_enable(WDTO_1S);
+
+    SET_HIGH(LED_PORT, LED_PIN);
+    while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN));
+  }
+  SET_HIGH(LED_PORT, LED_PIN);
+  //watchdogConfig(_BV(WDP1) | _BV(WDP0) | _BV(WDE)); // WATCHDOG_125MS
+  wdt_enable(WDTO_120MS);
+  while (1);
 }
--- src/tmp/watchdog/boot.h
+++ src/tmp/watchdog/boot.h
@@ -1,4 +1,3 @@
-#include <avr/boot.h>
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/wdt.h>
@@ -6,11 +5,3 @@
 #include "display.h"
 #include "macro.h"
 #include "pins.h"
-
-#ifndef BOOT_H                                         
-#define BOOT_H
-
-void setup_button_(void);
-void setup_led_(void);
-
-#endif
