25b6cad391ec14431547344c9eeb8d62bdd28b1f
Lis <andrei_chernov1@epam.com>
2020-03-04 19:48:27 +0100

    test watchdog

--- src/tmp/watchdog/boot.c
+++ src/tmp/watchdog/boot.c
@@ -4,6 +4,8 @@
 #define WATCHDOG_1S     (_BV(WDP2) | _BV(WDP1) | _BV(WDE))
 #define WATCHDOG_125MS  (_BV(WDP1) | _BV(WDP0) | _BV(WDE))
 
+const int (*app_start)(void) = 0x0;
+
 void setup_button_(void){
   SET_DDR_IN(BUTTON_C_DDR, BUTTON_C_PIN);
   SET_PULLUP(BUTTON_C_PORT, BUTTON_C_PIN);
@@ -21,20 +23,26 @@ void watchdog_config(uint8_t x) {
 
 int main(void){
   setup_button_();
-  setup_led_();
 
+  SP=RAMEND; // This is done by hardware reset
   if (MCUSR & _BV(EXTRF)) {
     MCUSR = ~(_BV(WDRF));
   }
 
+  // off
   watchdog_config(WATCHDOG_OFF);
 
   if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
+    setup_led_();
     SET_HIGH(LED_PORT, LED_PIN);
     while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN));
-  }
-  SET_HIGH(LED_PORT, LED_PIN);
+    SET_LOW(LED_PORT, LED_PIN);
+    _delay_ms(300);    
+    // load app
 
-  watchdog_config(WATCHDOG_125MS);
-  while (1);
+    // restart
+    watchdog_config(WATCHDOG_125MS);
+    while (1){};
+  }
+  app_start();
 }
--- src/tmp/watchdog/boot.h
+++ src/tmp/watchdog/boot.h
@@ -1,6 +1,5 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
-#include "display.h"
 #include "macro.h"
 #include "pins.h"
