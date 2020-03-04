a61c09e6b009e0f81a774b1e4bada9cd6eded199
Lis <andrei_chernov1@epam.com>
2020-03-04 18:11:17 +0100

    watchdog works

--- src/tmp/watchdog/boot.c
+++ src/tmp/watchdog/boot.c
@@ -1,5 +1,9 @@
 #include "boot.h"
 
+#define WATCHDOG_OFF    (0)                                                        
+#define WATCHDOG_1S     (_BV(WDP2) | _BV(WDP1) | _BV(WDE))
+#define WATCHDOG_125MS  (_BV(WDP1) | _BV(WDP0) | _BV(WDE))
+
 void setup_button_(void){
   SET_DDR_IN(BUTTON_C_DDR, BUTTON_C_PIN);
   SET_PULLUP(BUTTON_C_PORT, BUTTON_C_PIN);
@@ -10,13 +14,20 @@ void setup_led_(void){
   SET_LOW(LED_PORT, LED_PIN);
 }
 
+void watchdog_config(uint8_t x) {                                                   
+  WDTCSR = _BV(WDCE) | _BV(WDE);                                                   
+  WDTCSR = x;                                                                      
+}
+
 int main(void){
   setup_button_();
   setup_led_();
 
-  // WATCHDOG_OFF
-  //WDTCSR = _BV(WDCE) | _BV(WDE);
-  WDTCSR = 0;
+  if (MCUSR & _BV(EXTRF)) {
+    MCUSR = ~(_BV(WDRF));
+  }
+
+  watchdog_config(WATCHDOG_OFF);
 
   if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
     SET_HIGH(LED_PORT, LED_PIN);
@@ -24,9 +35,6 @@ int main(void){
   }
   SET_HIGH(LED_PORT, LED_PIN);
 
-  // WATCHDOG_125MS
-  WDTCSR = _BV(WDCE) | _BV(WDE);
-  WDTCSR = _BV(WDP1) | _BV(WDP0) | _BV(WDE);
-
+  watchdog_config(WATCHDOG_125MS);
   while (1);
 }
--- src/tmp/watchdog/boot.h
+++ src/tmp/watchdog/boot.h
@@ -1,6 +1,5 @@
 #include <util/delay.h>
 #include <avr/io.h>
-#include <avr/wdt.h>
 
 #include "display.h"
 #include "macro.h"
