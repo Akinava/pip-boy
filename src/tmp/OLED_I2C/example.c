2f520b0c5c7a36d08f19fc2e7a4df96cadf340d1
Lis <andrei_chernov1@epam.com>
2020-03-07 14:59:06 +0100

    dispaly init

---
+++ src/tmp/OLED_I2C/example.c
@@ -0,0 +1,7 @@
+#include "example.h"
+
+int main(void){
+  displayBegin();
+  print_char('B', x, y);
+  while(1);
+}
---
+++ src/tmp/OLED_I2C/example.h
@@ -0,0 +1,12 @@
+#include <avr/boot.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include "display.h"
+#include "macro.h"
+#include "pins.h"
+
+#ifndef DISPALY_H                                         
+#define DISPALY_H
+
+
+#endif
