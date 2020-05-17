01d38de46189b4aa445db1627abde53f5026cdfc
Lis <andrei_chernov1@epam.com>
2020-05-17 23:16:01 +0200

    1798 bytes / test requared

--- src/bootloader/boot.c
+++ src/bootloader/boot.c
@@ -7,9 +7,9 @@ int main(void){
   watchdog_config_(WATCHDOG_RESET);
 
   // MAIN LOGIC
+  setup_button_();
   if(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){
     setup_led_();
-
     SET_HIGH(LED_PORT, LED_PIN);
 
     while(CHECK_PIN(BUTTON_C_PINS, BUTTON_C_PIN)){};
@@ -26,41 +26,54 @@ void load_default_app(void){
 
 void load_app_by_name(const char* file_path){
   setup_led_();
+
   if (!sd_init()){
     error_light_();
   }
 
-  file_t boot_file;
-  if (!find_file_by_path(file_path, &boot_file)){
+  if (!find_file_by_path(file_path)){
     error_blink_();
   }
 
-  load_app_by_cluster(boot_file.cluster, boot_file.size);
+  load_app_by_cluster(file.cluster, file.size);
 }
 
 void load_app_by_cluster(uint16_t cluster, uint32_t size){
   setup_led_();
+
   if (!sd_init()){
     error_light_();
   }
-
-  file_t boot_file;
-  boot_file.cluster = cluster;
-  boot_file.size = size;
-  boot_file.cursor = 0;
-  get_sector_by_cluster_(&boot_file);
+  uint32_t sector = get_sector_by_cluster_(cluster);
 
   uint32_t address = 0;
 
-  while(boot_file.cursor < boot_file.size){
-    if (!file_read_sector(&boot_file)){
-      error_blink_();
+  while(address < size && address % SPM_PAGESIZE == 0 && address != 0){
+    if (address % SECTOR_BUFFER_SIZE == 0){
+    // read file sector
+      if (!read_sector_(file.sector)){error_blink_();}
+      sector++;
     }
-    for (uint8_t page=0; page<vol_info.bytes_per_sector/SPM_PAGESIZE; page++){
-      block_flash_load_(&address, page);
-      if (address + SPM_PAGESIZE*page > boot_file.size){break;}
+    // write page
+    if (address % SPM_PAGESIZE == 0 && address != 0){
+	    // Perform page erase
+      //boot_page_erase(address);
+	    // Wait until the memory is erased
+      //boot_spm_busy_wait();		
+
+      boot_page_write(address-SPM_PAGESIZE);
+	    // wait until finished writing
+      boot_spm_busy_wait();
+	    // Re-enable the RWW section 
+      //boot_rww_enable();
     }
+
+    // add word
+    uint16_t temp_word = *((uint16_t*)(sector_buffer + address));
+    boot_page_fill(address, temp_word);
+    address += 2;
   }
+
   SET_HIGH(LED_PORT, LED_PIN);
   _delay_ms(100);
   SET_LOW(LED_PORT, LED_PIN);
@@ -69,24 +82,6 @@ void load_app_by_cluster(uint16_t cluster, uint32_t size){
   watchdog_config_(WATCHDOG_125MS);
 	while (1);
  }
-
-void block_flash_load_(uint32_t* address, uint8_t page){
-	// Perform page erase
-  //boot_page_erase(address);
-	// Wait until the memory is erased
-  //boot_spm_busy_wait();		
-
-  for(uint8_t i = 0; i < SPM_PAGESIZE; i+=2){
-		uint16_t temp_word = *((uint16_t*)(sector_buffer + i + page * SPM_PAGESIZE));
-    boot_page_fill(*address + i, temp_word);;
-  } 
-  boot_page_write(*address);
-	// wait until finished writing
-  boot_spm_busy_wait();
-	// Re-enable the RWW section 
-  //boot_rww_enable();
-  *address += SPM_PAGESIZE;
-}
 
 void setup_button_(void){
   SET_DDR_IN(BUTTON_C_DDR, BUTTON_C_PIN);
@@ -99,18 +94,260 @@ void setup_led_(void){
 }
 
 void error_light_(void){
-    SET_HIGH(LED_PORT, LED_PIN);
-    while(1){}
+  SET_HIGH(LED_PORT, LED_PIN);
+  while(1);
 }
 
 void error_blink_(void){
   while(1){
     TOGGLE(LED_PORT, LED_PIN);
-    _delay_ms(1000);
+    _delay_ms(500);
   }
 }
 
 void watchdog_config_(uint8_t x){
   WDTCSR = _BV(WDCE) | _BV(WDE);
   WDTCSR = x;
+}
+
+// ===== SD ===== //
+
+uint8_t find_file_by_path(const char* file_path){
+  // parameters:
+  // file_path - in unix view exp: '/BIN/APP.BIN'
+  // file      - file sector, file size
+  //
+  // if file exist save in file file sector and size; return 1
+  // else retrn 0
+
+  file.sector = root_sector_;
+  file.cluster = ROOT_CLUSTER;
+  uint8_t file_path_cursor = 1; // zero sybmol is a slash '/'
+
+  while(copy_file_name(&file_path_cursor, file_path)){
+    if (!find_obj_by_name()){
+      return 0;
+    }
+  }
+  if (sizeof(file_path) == file_path_cursor){
+    return 1;
+  }
+  return 0;
+}
+
+uint8_t copy_file_name(uint8_t* file_path_cursor, const char* file_path){
+  uint8_t c = pgm_read_byte(file_path + *file_path_cursor);
+  if (c == 0){return 0;}
+  
+  erase_obj_name_();
+  uint8_t name_index = 0;
+
+
+  while(c || c != CHAR_SLASH){
+    if (c == CHAR_DOT){
+      name_index = OBJECT_NAME_SIZE;
+    }else{
+      *(obj_name_ + name_index) = c;
+      name_index++;
+    }
+    (*file_path_cursor)++;
+    c = pgm_read_byte(file_path + *file_path_cursor);
+  }
+  return 1;
+}
+
+uint8_t find_obj_by_name(void){
+  uint16_t sector_offset;
+
+  do{
+    if(!read_sector_(file.sector)){
+      return 0;
+    }
+    // go through sector
+    for (sector_offset=0; sector_offset<vol_info.bytes_per_sector; sector_offset+=OBJECT_RECORD_SIZE){
+      if(compare_name(sector_buffer+sector_offset)){
+        return 1;
+      }
+    }
+  }while(next_sector());
+
+  return 0;
+}
+
+void erase_obj_name_(void){
+  for (uint8_t i=0; i<OBJECT_NAME_SIZE+OBJECT_EXT_SIZE; i++){
+    obj_name_[i] = CHAR_SPACE;
+  }
+}
+
+uint8_t compare_name(uint8_t* buf){
+  for (uint8_t i=0; i<OBJECT_NAME_SIZE+OBJECT_EXT_SIZE; i++){
+    if (*(buf+i) != *(obj_name_+i)){return 0;}
+  }
+  return 1;
+}
+
+uint8_t next_sector(void){
+  file.cluster++;
+  if (file.cluster == ROOT_CLUSTER){
+    //                 /                                     root sectors                                            /
+    if (file.sector <= root_sector_ + vol_info.root_directory_entries * OBJECT_RECORD_SIZE / vol_info.bytes_per_sector){
+      return 1;
+    }
+  }else{
+    //    last sector in cluster
+    if (get_sector_by_cluster_(file.cluster) + vol_info.sectors_per_claster <= file.sector){
+      return 1;
+    }else{
+      if(next_cluster_()){
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+uint8_t next_cluster_(void){
+  uint16_t fat_cluster_place = file.cluster * sizeof(file.cluster);
+  if (!read_sector_(fat_sector_)){return 0;}
+  file.cluster = *((uint32_t*)(sector_buffer + fat_cluster_place));
+  if (file.sector >= END_OF_CLASTERCHAIN){return 0;}
+  file.sector = get_sector_by_cluster_(file.cluster);
+  return 1;
+}
+
+uint32_t get_sector_by_cluster_(uint16_t cluster){
+  return data_sector_ + ((cluster-2) * vol_info.sectors_per_claster);
+}
+
+//============================== sd func ====================================//
+
+uint8_t sd_init(void){
+  if (!card_init_()){return 0;}
+  if (!vol_init_()){return 0;}
+  return 1;
+}
+
+uint8_t vol_init_(void){
+  // volume address
+  if(!read_sector_(0)){return 0;}
+  volume_sector_ = *((uint32_t*)(sector_buffer + VOL_ADDRESS_OFFSET));
+  // volume info
+  if(!read_sector_(volume_sector_)){return 0;}
+  vol_info = *((vol_info_t*)(sector_buffer + VOL_INFO_OFFSET));
+
+  fat_sector_ = volume_sector_ + vol_info.reserved_sectors; 
+  root_sector_ = fat_sector_ + vol_info.sectors_per_FAT * vol_info.number_of_FATs;
+  data_sector_ = root_sector_ + vol_info.root_directory_entries * OBJECT_RECORD_SIZE / vol_info.bytes_per_sector;
+  return 1;
+}
+
+uint8_t card_init_(void){
+  // Set MOSI, SCK, CS as Output
+  SD_DDR |= _BV(MOSI)|_BV(SCK)|_BV(SD_CS);
+  // set sd cs off
+  SD_UNSET(SD_PORT, SD_CS);
+  // Enable SPI, Set as Master
+  //Prescaler: Fosc/16, Enable Interrupts
+  SPCR = _BV(SPE)|_BV(MSTR)|_BV(SPR0); // | (1 << SPR1)
+
+  for (uint8_t i=0; i<10; i++){
+    spi_send_(0xFF);
+  }
+
+  card_command_(CMD0, 0, 0x95);
+  for (uint16_t retry = 0; ;retry++){
+  if (retry == 0xFFFF) {
+      return 0;
+    }
+    if (SPDR == R1_IDLE_STATE){
+      break;
+    }
+    spi_send_(0xFF);
+  }
+
+  card_command_(CMD8, 0x01AA, 0x87);
+  if (SPDR != 1){
+    return 0;
+  }
+ 
+  for (uint16_t retry = 0; ; retry++) {
+    card_command_(CMD55, 0, 0xFF);
+    card_command_(ACMD41, 0x40000000, 0xFF);
+    if (SPDR == R1_READY_STATE)break;
+    if (retry == 1000) {
+      return 0;
+    }
+  }
+
+  card_command_(CMD58, 0, 0xFF );
+  if(SPDR){
+      return 0;
+  }
+
+  for (uint8_t i = 0; i < 4; i++){
+    spi_send_(0xFF);
+  }
+  
+  //use max SPI frequency
+  SPCR &= ~((1 << SPR1) | (1 << SPR0)); // f_OSC/4
+  SPSR |= (1 << SPI2X); // Doubled Clock Frequency: f_OSC/2
+  SD_UNSET(SD_PORT, SD_CS);
+  return 1;
+}
+
+void spi_send_(uint8_t data){
+  SPDR = data;
+  while(!(SPSR & (1<<SPIF)));
+}
+
+void card_command_(uint8_t cmd, uint32_t arg, uint8_t crc){
+  // end read if in partialBlockRead mode
+  spi_send_(0xFF);
+  //select card
+  SD_SET(SD_PORT, SD_CS);
+  // send command
+  spi_send_(cmd | 0x40);
+  //send argument
+  for (int8_t s = 24; s >= 0; s -= 8){
+    spi_send_(arg >> s);
+  }
+  //send CRC
+  spi_send_(crc);
+  //wait for not busy
+  spi_send_(0xFF);
+  for (uint8_t retry = 0; retry < 0xFF; retry++){
+    spi_send_(0xFF);
+    if (SPDR != 0xFF){
+      break;
+    }
+  }
+}
+
+uint8_t wait_start_block_(void){
+  uint16_t retry = 10000;
+  do{
+    spi_send_(0xFF);
+    if (SPDR == DATA_START_BLOCK) return 1;
+  }while(retry);
+  return 0;
+}
+
+uint8_t read_sector_(uint32_t sector){
+  card_command_(CMD17, sector, 0xFF);
+  if (SPDR || !wait_start_block_()){
+    return 0;
+  }
+
+  // read sector
+  for (uint16_t i = 0; i < 512; i++) {
+    spi_send_(0xFF);
+    sector_buffer[i] = SPDR;
+  }
+
+  //read 2 bytes CRC (not used)
+  spi_send_(0xFF);
+  spi_send_(0xFF);
+  SD_UNSET(SD_PORT, SD_CS);
+  return 1;
 }
--- src/bootloader/boot.h
+++ src/bootloader/boot.h
@@ -1,13 +1,50 @@
+#include <avr/pgmspace.h>
 #include <avr/boot.h>
 #include <util/delay.h>
-#include <avr/io.h>
-#include "macro.h"
-#include "pins.h"
-#include "sd.h"
 
-#ifndef BOOT_H                                         
-#define BOOT_H
 
+// defeine MACRO
+// DDR Data Direction Register                                                  
+#define SET_DDR_OUT(DDR, PIN) DDR |= _BV(PIN)                                   
+#define SET_DDR_IN(DDR, PIN) DDR &= ~ _BV(PIN)                                  
+#define SET_PULLUP(PORTS, PIN) PORTS |= _BV(PIN)                                
+                                                                                 
+// PORTS Data Register                                                          
+#define SET_HIGH(PORTS, PIN) PORTS |= _BV(PIN)                                  
+#define SET_LOW(PORTS, PIN) PORTS &= ~ _BV(PIN)                                 
+#define TOGGLE(PORTS, PIN) PORTS ^= _BV(PIN);                                   
+                                                                               
+// PINS Input Pins Register                                                     
+#define CHECK_PIN(PINS, PIN) !(PINS & _BV(PIN))                                 
+
+
+// define PINS
+// DEFINE BUTTON C                                                                 
+#define BUTTON_C_DDR DDRC                                                         
+#define BUTTON_C_PORT PORTC                                                       
+#define BUTTON_C_PINS PINC                                                        
+#define BUTTON_C_PIN PC3                                                     
+
+// DEFINE LED
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+#define LED_PIN PD5
+
+// DISPLAY
+#define DISPLAY_DDR DDRC
+#define DISPLAY_PORT PORTC
+#define SDA PC4
+#define SCL PC5
+
+// SD
+#define SD_DDR DDRB
+#define SD_PORT PORTB
+#define SD_CS PB2
+#define MOSI PB3
+#define MISO PB4
+#define SCK PB5
+
+// define BOOT
 #define WATCHDOG_RESET  (_BV(WDIE) | _BV(WDP2) | _BV(WDP1))
 #define WATCHDOG_125MS  (_BV(WDP1) | _BV(WDP0) | _BV(WDE))
 
@@ -17,11 +54,108 @@ const int (*app_start)(void) = 0x0;
 void load_default_app(void);
 void load_app_by_name(const char* file_path);
 void load_app_by_cluster(uint16_t cluster, uint32_t size);
-void block_flash_load_(uint32_t* address, uint8_t page);
+
 void setup_button_(void);
 void setup_led_(void);
 void error_light_(void);
 void error_blink_(void);
 void watchdog_config_(uint8_t x);
 
-#endif
+// SD card commands
+/** GO_IDLE_STATE - init card in spi mode if CS low */
+#define CMD0     0      
+/** SEND_IF_COND - verify SD Memory Card interface operating condition.*/
+#define CMD8     8
+/** APP_CMD - escape for application specific command */
+#define CMD55    55 
+/** READ_BLOCK - read a single data block from the card */
+#define CMD17    17
+/** SD_SEND_OP_COMD - Sends host capacity support information and
+    activates the card's initialization process */
+#define ACMD41   41  
+/** READ_OCR - read the OCR register of a card */
+#define CMD58    58
+
+/** status for card in the ready state */
+#define R1_READY_STATE 0
+/** status for card in the idle state */
+#define R1_IDLE_STATE 1
+
+/** start data token for read or write */
+#define DATA_START_BLOCK 0xfe
+
+#define VOL_ADDRESS_OFFSET 0x01c6
+
+#define VOL_INFO_OFFSET 0x0b
+
+#define FILE_CLUSTER_OFFSET 0x1a
+#define FILE_SIZE_OFFSET 0x1c
+
+#define OBJECT_RECORD_SIZE 32
+#define OBJECT_NAME_SIZE    8
+#define OBJECT_EXT_SIZE     3
+
+#define END_OF_CLASTERCHAIN 0x0fff8
+
+#define CHAR_SLASH 0x2f
+#define CHAR_DOT 0x2e
+#define CHAR_SPACE 0x20
+
+#define SD_SET SET_LOW
+#define SD_UNSET SET_HIGH
+
+#define SECTOR_BUFFER_SIZE 512
+
+#define ROOT_CLUSTER 0
+
+typedef struct {
+  uint32_t sector;
+  uint32_t size;
+  uint16_t cluster;
+} file_t;
+
+file_t file;
+
+uint8_t sector_buffer[SECTOR_BUFFER_SIZE];
+
+typedef struct {
+  uint16_t bytes_per_sector;
+  uint8_t sectors_per_claster;
+  uint16_t reserved_sectors;
+  uint8_t number_of_FATs;
+  uint16_t root_directory_entries;
+  uint16_t total_logical_sectors;
+  uint8_t media_descriptor;
+  uint16_t sectors_per_FAT; 
+} vol_info_t;
+
+vol_info_t vol_info;
+
+uint8_t sd_init(void);
+uint8_t read_file_sector(file_t* file);
+uint8_t find_file_by_path(const char* file_path);
+
+uint8_t next_cluster_(void);
+uint8_t copy_file_name(uint8_t* cursor, const char* file_path);
+uint8_t next_claster_(void);
+uint8_t compare_name(uint8_t* buf);
+uint8_t next_sector(void);
+uint8_t card_init_(void);
+uint8_t vol_init_(void);
+uint8_t find_obj_by_name(void);
+uint32_t get_sector_by_cluster_(uint16_t cluster);
+uint8_t check_obj_has_name_(void);
+void cp_record_data_(uint8_t* buffer);
+void erase_obj_name_(void);
+void file_info_parce_(file_t* file, uint8_t* file_info);
+void spi_send_(uint8_t data);
+void card_command_(uint8_t cmd, uint32_t arg, uint8_t crc);
+uint8_t wait_start_block_(void);
+uint8_t read_sector_(uint32_t sector);
+
+static uint32_t volume_sector_;
+static uint32_t fat_sector_;
+static uint32_t root_sector_;
+static uint32_t data_sector_;
+
+static uint8_t obj_name_[OBJECT_NAME_SIZE+OBJECT_EXT_SIZE];
